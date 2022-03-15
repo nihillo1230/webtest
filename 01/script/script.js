dc0cb897c3b3c272d176d8af964d6da34fe50598
Mikyoung <nihillo@hanmail.net>
2022-03-16 00:59:33 +0900

    https://github.com/nihillo70/webtest.git

---
+++ 01/script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("header nav ul.gnb>li").hover(function(){
+        $("ul.sub").stop().slideDown();
+    },function(){
+        $("ul.sub").stop().slideUp();
+    });
+
+
+}); //ready end
